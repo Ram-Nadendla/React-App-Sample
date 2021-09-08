8d90e73a348aca7e6f2beebf5735c8184d8be977
Ram-Nadendla <ramnadendla1111@gmail.com>
2021-09-08 17:49:58 +0530

    Adding routes for the login page & dashboard

--- src/Routers.js
+++ src/Routers.js
@@ -1,70 +1,59 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
 } from "react-router-dom";
 import './Main.css';
-//import Login from './Login';
-import TodoList  from "./TodoList";
-    import SignUp from './SignUp';
-    import Table from './Table';
-    import LineChart from './LineChart';	
- 
+import Login from './Login';
+import SignUp from './SignUp';
+import Table from './Table';
+//   import LineChart from './LineChart';	
+
 export default function Routers() {
-    return (
-      <Router>
-        <div>
-          <ul>
-  <li>
-  <Link to="/">
-      <a class="active" href="#home">
-          Login
-          </a>
-          </Link>
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">
+              <a class="active" href="#home">
+                Login
+              </a>
+            </Link>
           </li>
-         
-  <li>
-  <Link to="/news">
-      <a href="#news">
-          Register
-          </a>
-          </Link>
+
+          <li>
+            <Link to="/signUp">
+              <a href="#signUp">
+                Register
+              </a>
+            </Link>
           </li>
           <li>
-  <Link to="/table">
-      <a href="#table">
-      Dashboard
-          </a>
-          </Link>
+            <Link to="/dashboard">
+              <a href="#dashboard">
+                Dashboard
+              </a>
+            </Link>
           </li>
-          <li>
-  <Link to="/chart">
-      <a href="#chart">
-      Chart
-          </a>
-          </Link>
-          </li>
-</ul>
-  
-  
-          
-          <Switch>
-            <Route exact path="/">
-             <TodoList/>
-            </Route>
-            <Route path="/news">
-              <SignUp />
-            </Route>
-            <Route path="/table">
-              <Table />
-            </Route>
-            <Route path="/chart">
-              <LineChart />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+        </ul>
+
+
+
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/signUp">
+            <SignUp />
+          </Route>
+          <Route path="/dashboard">
+            <Table />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
